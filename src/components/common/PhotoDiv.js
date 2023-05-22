2318064f893799fc3a05c12dc878777f41a2a623
Tarikkoc <ahmettarikkoc@gmail.com>
2023-05-22 12:06:03 +0300

    tk2012

--- src/components/common/PhotoDiv.js
+++ src/components/common/PhotoDiv.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const PhotoDiv = ({ title, img }) => {
   return (
-    <div className="mt-0.5 bg-slate-200 menu-background bg-cover flex border-b-2 border-slate-300 photoDiv">
-      <div className="w-1/4 pt-10 text-4xl font-normal text-orange-500 text-right">
-        {title}
-      </div>
-      <div className="w-9/12 flex justify-center items-center ">
-        <img
-          className="min-h-[50px] min-w-[200px] w-[600px]"
-          src={img}
-          alt=""
-        />
+    <div className=" bg-slate-200 flex justify-center menu-background bg-cover border-b-2 border-slate-300 ">
+      <div className="container mx-auto mt-0.5 photoDiv justify- justify-center flex">
+        <div className="w-1/4 pt-10  text-4xl font-normal text-orange-500 ">
+          {title}
+        </div>
+        <div className="w-1/2 flex justify-center items-center ">
+          <img
+            className="min-h-[50px] min-w-[200px] w-[600px]"
+            src={img}
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );
